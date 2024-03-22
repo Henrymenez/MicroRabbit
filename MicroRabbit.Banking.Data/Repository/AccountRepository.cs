a23f8530c5d0e39e409c75d8a3a217eaf52e6aac
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2024-03-22 14:48:40 +0100

    working on banking microservice

---
+++ MicroRabbit.Banking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,25 @@
+﻿using MicroRabbit.Banking.Data.Context;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly BankindDbContext _bankingDb;
+
+        public AccountRepository(BankindDbContext bankingDb)
+        {
+           _bankingDb = bankingDb;
+        }
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _bankingDb.Accounts;
+        }
+    }
+}
