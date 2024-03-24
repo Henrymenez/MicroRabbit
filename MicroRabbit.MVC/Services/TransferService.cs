cf364d8a6c82cad58d99642b1918a4b8a5548a11
Henry Ugochukwu <henryugochukwuagu@gmail.com>
2024-03-24 18:02:58 +0100

    done

---
+++ MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,25 @@
+﻿using MicroRabbit.MVC.Models.Dto;
+using Newtonsoft.Json;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _httpClient;
+
+        public TransferService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task Transfer(TransferDto transferDto)
+        {
+            var url = "https://localhost:7276/api/Banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto),
+                System.Text.Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync(url, transferContent);
+            response.EnsureSuccessStatusCode();
+
+
+        }
+    }
+}
